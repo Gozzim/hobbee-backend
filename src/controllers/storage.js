5e75980eba6c959f79855d25d6b7a488b2b253f8
Kempec Halk <kempec.halk@tum.de>
2021-07-01 23:19:30 +0200

    feat(Notifications): Add support for notifications

---
+++ src/controllers/storage.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const datefns = require("date-fns");
+
+const NotificationModel = require("../models/notification");
+
+const getUserNotifications = async (req, res) => {
+    try {
+        const since = datefns.parseISO(req.query.since);
+        const response = await NotificationModel.find({
+            user: req.userId,
+            date: {$gt: since}
+        }).select("_id date read content").sort("date").exec();
+
+        return res.status(200).json(response);
+    } catch (e) {
+        return res.status(500).json({
+            error: "Internal Server Error",
+            message: e.message,
+        });
+    }
+}
+
+module.exports = {
+    getUserNotifications,
+};
