0527f723a337be5bb7c8f3e7c4ae27859b193ab1
Zaim Sari <zaim.sari@tum.de>
2021-07-19 18:16:02 +0200

    feat(updateMe): updateMe function for user initiated profile updates

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,21 +1,14 @@
 "use strict";
 
 const bcrypt = require("bcryptjs");
-const crypto = require("crypto");
 
-const config = require("../config");
 const UserModel = require("../models/user");
-const ResetTokenModel = require("../models/resetToken");
 const { generateToken } = require("../shared/helpers");
 const { renewSubscription } = require("./payment");
 const { ERRORS } = require("../shared/Constants");
 const { isValidEmail } = require("../validators/auth");
 const { errorHandler } = require("../middlewares");
-const {
-  sendResetPassword,
-  sendConfirmChange,
-  sendAccountConfirmation,
-} = require("../services/mail");
+const { sendAccountConfirmation } = require("../services/mail");
 const { isValidPassword } = require("../validators/auth");
 
 const login = async (req, res) => {
@@ -42,7 +35,6 @@ const login = async (req, res) => {
         "username password premium.active premium.subscription.expiration premium.subscription.id"
       );
     }
-    //console.log(user);
 
     // check if the password is valid
     const isPasswordValid = bcrypt.compareSync(
@@ -149,140 +141,6 @@ const register = async (req, res) => {
   }
 };
 
-const forgotPassword = async (req, res) => {
-  // Check if body contains required properties
-  const error = errorHandler(req, res, ["email"]);
-  if (error) {
-    return error;
-  }
-
-  try {
-    const user = await UserModel.findOne({ email: req.body.email }).exec();
-
-    if (user) {
-      let token = await ResetTokenModel.findOne({ user: user._id });
-      if (token) {
-        await token.delete();
-      }
-
-      const resetToken = crypto.randomBytes(32).toString("hex");
-      // Hashing Token. Saving plain token is comparable to saving plain passwords
-      const tokenHash = await bcrypt.hash(resetToken, 8);
-      const newToken = {
-        user: user._id,
-        token: tokenHash,
-      };
-      await ResetTokenModel.create(newToken);
-
-      // Generate link with userId and token for validation while resetting the password
-      const link = `${config.frontendDomain}/password-reset/${user._id}/${resetToken}`;
-      await sendResetPassword(user, link);
-    }
-
-    return res.status(200).send({});
-  } catch (err) {
-    return res.status(500).json({
-      error: "Internal server error",
-    });
-  }
-};
-
-const resetPassword = async (req, res) => {
-  // Check if body contains required properties
-  const error = errorHandler(req, res, ["user", "token", "password"]);
-  if (error) {
-    return error;
-  }
-
-  try {
-    let resetToken = await ResetTokenModel.findOne({ user: req.body.user });
-    if (!resetToken) {
-      return res.status(400).json({
-        error: "Bad Request",
-        message: ERRORS.invalidRecoveryToken,
-      });
-    }
-
-    const isValidToken = bcrypt.compareSync(req.body.token, resetToken.token);
-    if (!isValidToken) {
-      return res.status(400).json({
-        error: "Bad Request",
-        message: ERRORS.invalidRecoveryToken,
-      });
-    }
-
-    // Password validation before hashing
-    const isPassValid = await isValidPassword(req.body.password);
-    if (!isPassValid) {
-      return res.status(400).json({
-        error: "Bad Request",
-        message: ERRORS.invalidPassword,
-      });
-    }
-
-    // hash the password before storing it in the database
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
-    const user = await UserModel.findById(req.body.user).select(
-      "username premium.active premium.subscription.expiration premium.subscription.id"
-    );
-    if (!user) {
-      // Should never happen
-      return res.status(404).json({
-        error: "Not Found",
-        message: ERRORS.userNotFound,
-      });
-    }
-    user.password = hashedPassword;
-
-    // Finalize modifications and send confirmation mail
-    await user.save();
-    await resetToken.delete();
-    await sendConfirmChange(user);
-
-    // Check premium status to prevent abuse
-    if (user.premium.active) {
-      const premiumExpiration = Date.parse(
-        user.premium.subscription.expiration
-      );
-      if (Date.now() > premiumExpiration) {
-        user.premium = await renewSubscription(user.premium.subscription.id);
-        await user.save();
-      }
-    }
-
-    // Generate JWT token to log user in again right away
-    const token = await generateToken(user);
-    res.status(200).json({
-      token: token,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      error: "Internal server error",
-    });
-  }
-};
-
-const me = async (req, res) => {
-  try {
-    // get own user from database
-    let user = await UserModel.findById(req.userId).exec();
-
-    if (!user) {
-      return res.status(404).json({
-        error: "Not Found",
-        message: ERRORS.userNotFound,
-      });
-    }
-
-    return res.status(200).json(user);
-  } catch (err) {
-    return res.status(500).json({
-      error: "Internal Server Error",
-      message: err.message,
-    });
-  }
-};
-
 const isUsernameAvailable = async (req, res) => {
   // Check if body contains required properties
   const error = errorHandler(req, res, ["username"]);
@@ -315,8 +173,5 @@ module.exports = {
   login,
   register,
   logout,
-  forgotPassword,
-  resetPassword,
-  me,
   isUsernameAvailable,
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,17 +4,14 @@ const express = require("express");
 const router = express.Router();
 
 const middlewares = require("../middlewares");
-const AuthController = require("../controllers/auth");
 const UserController = require("../controllers/user");
-
-router.post("/login", AuthController.login); // login
-router.post("/register", AuthController.register); // register a new user
-router.post("/logout", middlewares.checkAuthentication, AuthController.logout); // logout user
-router.post("/exists/username", AuthController.isUsernameAvailable);
 
 
 router.post("/forgot", UserController.forgotPassword); // Send reset password link to user
 router.post("/reset", UserController.resetPassword); // Change user password from reset link
+router.post("/update", middlewares.checkAuthentication, UserController.updateMe); // Update userdata from profile, requires a logged in user
+
+router.get("/me", middlewares.checkAuthentication, UserController.me); // get own username, requires a logged in user
 
 
 module.exports = router;
